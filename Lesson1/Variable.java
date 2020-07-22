e7ff8c3e2dd7b1b895bf6bfc66ca4367b57fbf9f
Enekeen <enekeen@gmail.com>
2020-07-23 00:48:39 +1000

    HomeTask 1

---
+++ Lesson1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+	public static void main(String[] args) {
+		boolean choice = true;
+		byte laptop = 1;
+		short ram = 8;
+		int ssdDrive = 1000;
+		long productCode = 1033110000000001l;
+		char studyFolderDrive = 'D';
+		float priceUS = 1.99999f;
+		double workHours = 100.239876540d;
+
+		System.out.println(choice);
+		System.out.println(laptop);
+		System.out.println(ram);
+		System.out.println(ssdDrive);
+		System.out.println(productCode);
+		System.out.println(studyFolderDrive);
+		System.out.println(priceUS);
+		System.out.println(workHours);
+
+	}
+}
