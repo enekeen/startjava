620ccdb077cdd796784326bff09e3752d1878913
Enekeen <enekeen@gmail.com>
2020-07-27 20:36:23 +1000

    HomeTask4 Calculator

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int num1 = 7;
+		int num2 = 3;
+		int result = 1;
+		char operator = '^';
+
+		if (operator == '+') {
+			result = num1 + num2;
+		} else if (operator == '-') {
+			result = num1 - num2;
+		} else if (operator == '*') {
+			result = num1 * num2;
+		} else if (operator == '/') {
+			result = num1 / num2;
+		} else if (operator == '^') {
+			for (int i = 1; i <= num2; i++) {
+				result = result*num1;
+			}
+		} else if (operator =='%') {
+			result = num1 % num2;
+		}
+
+		System.out.println(result);
+	}
+}
