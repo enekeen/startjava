bc2914036275e62621eb7bb37b7dca0b66611f1c
Enekeen <enekeen@gmail.com>
2020-07-29 22:23:23 +1000

    Calculator. Check if num=0

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -14,6 +14,9 @@ public class Calculator {
 		} else if (operator == '/') {
 			result = num1 / num2;
 		} else if (operator == '^') {
+			if (num2 == 0) {
+				System.out.println(1);
+			}
 			for (int i = 1; i <= num2; i++) {
 				result *= num1;
 			}
