20754641ab0e87b6a36c3a9d04c9f99a2a954f81
Enekeen <enekeen@gmail.com>
2020-07-30 23:06:49 +1000

    HomeTask2. Wolf.

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,30 @@
+public class Wolf {
+	
+	/*String sex = "male";
+	String nickname = "Fenrir";
+	int weight = 900;
+	int age = 3000;
+	String color = "grey";*/
+	String sex, nickname, color;
+	int weight, age;
+
+	void walk() {
+		System.out.println("Wolf is walking.");
+	}
+
+	void sit() {
+		System.out.println("Wolf is sitting.");
+	}
+
+	void run() {
+		System.out.println("Wolf is running.");
+	}
+
+	void howl() {
+		System.out.println("Auuuuuuuuuuu!");
+	}
+
+	void hunt() {
+		System.out.println("Wolf is hunting.");
+	}
+}
---
+++ Lesson2/WolfTest.java
@@ -0,0 +1,24 @@
+public class WolfTest {
+	public static void main (String[] args) {
+		
+		Wolf wereWolf = new Wolf();
+		
+		wereWolf.sex = "male";
+		wereWolf.nickname = "Fenrir";
+		wereWolf.color = "grey";
+		wereWolf.weight = 1000;
+		wereWolf.age = 3000;
+
+		System.out.println("Wolf sex is " + wereWolf.sex);
+		System.out.println("Wolf nickname is " + wereWolf.nickname);
+		System.out.println("Wolf color is " + wereWolf.color);
+		System.out.println("Wolf weight is " + wereWolf.weight);
+		System.out.println("Wolf age is " + wereWolf.age);
+		
+		wereWolf.walk();
+		wereWolf.sit();
+		wereWolf.run();
+		wereWolf.howl();
+		wereWolf.hunt();
+	}
+}
