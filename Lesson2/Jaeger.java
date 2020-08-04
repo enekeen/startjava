6f9a04c3cec3e7b22805306871a4898768472d5d
Enekeen <enekeen@gmail.com>
2020-08-04 17:14:57 +1000

    Jaeger. Setters and getters.

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,82 @@
+public class Jaeger {
+
+	private String modelName;
+	private String mark;
+	private double height;
+	private double weight;
+	private int speed;
+	private	int strength;
+	private int armor;
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+
+	public int getArmor() {
+		return armor;
+	}
+
+	void drift() {
+		System.out.println("Jaeger is drifting");
+	}
+
+	void move() {
+		System.out.println("Jaeger is moving");
+	}
+
+	void scanKaiju() {
+		System.out.println("Scanning Kaiju");
+	}
+
+	void useVortexCannon() {
+		System.out.println("Jaeger is using vortex cannon");
+	}
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,39 @@
+public class JaegerTest {
+	
+	public static void main(String[] args) {
+		Jaeger strikerEureka = new Jaeger();
+		Jaeger chernoAlpha = new Jaeger();
+		/*
+		private String modelName;
+		private String mark;
+		private float height;
+		private float weight;
+		private int speed;
+		private	int strength;
+		private int armor;
+		*/
+
+		strikerEureka.setModelName("Striker Eureka");
+		strikerEureka.setMark("Mark-5");
+		strikerEureka.setHeight(76.2);
+		strikerEureka.setWeight(1.850);
+		strikerEureka.setSpeed(10);
+		strikerEureka.setStrength(10);
+		strikerEureka.setArmor(9);
+
+		chernoAlpha.setModelName("Cherno Alpha");
+		chernoAlpha.setMark("Mark-1");
+		chernoAlpha.setHeight(85.34);
+		chernoAlpha.setWeight(2.412);
+		chernoAlpha.setSpeed(3);
+		chernoAlpha.setStrength(10);
+		chernoAlpha.setArmor(10);
+
+		System.out.println(strikerEureka.getModelName());
+		System.out.println(chernoAlpha.getMark());
+		chernoAlpha.setSpeed(5);
+		System.out.println(chernoAlpha.getSpeed());
+		strikerEureka.setWeight(2.777);
+		System.out.println(strikerEureka.getWeight());
+	}
+}
