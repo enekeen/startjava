0e5454eec31bb675f0a194f02259a6370fc59fe0
Enekeen <enekeen@gmail.com>
2020-10-16 19:53:13 +1000

    Calculator using Array

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,55 +1,30 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.lang.Math.*;
+
 public class Calculator {
-	private int num1;
-	private int num2;
-	private char operation;
-	private int result;
+    private String[] strExp;
+    private int num1;
+    private int num2;
+    private char operation;
+    private int result;
 
-	public int getNum1() {
-		return num1;
-	}
+    public void setExpression(String expression) {
+        this.strExp = expression.split(" ");
+        this.num1 = Integer.parseInt(strExp[0]);
+        this.num2 = Integer.parseInt(strExp[2]);
+        this.operation = strExp[1].charAt(0);
+    }
 
-	public void setNum1(int num1) {
-		this.num1 = num1;
-	}
-
-	public int getNum2() {
-		return num2;
-	}
-
-	public void setNum2(int num2) {
-		this.num2 = num2;
-	}
-
-	public void setOperation(char operation) {
-		this.operation = operation;
-	}
-
-	public int calculate() {
-		switch (operation) {
-			case '+':
-				result = num1 + num2;
-				break;
-			case '-':
-				result = num1 - num2;
-				break;
-			case '*':
-				result = num1 * num2;
-				break;
-			case '/':
-				result = num1 / num2;
-				break;
-			case '^':
-				result = 1;
-				for (int i = 1; i <= num2; i++) {
-					result *= num1;
-				}
-				break;
-			case '%':
-				result = num1 % num2;
-				break;
-		}
-		return result;
-	}
+    public int calculate() {
+        switch (operation) {
+            case '+' -> result = Math.addExact(num1, num2);
+            case '-' -> result = Math.subtractExact(num1, num2);
+            case '*' -> result = Math.multiplyExact(num1, num2);
+            case '/' -> result = Math.floorDiv(num1, num2);
+            case '^' -> result = (int) Math.pow(num1, num2);
+            case '%' -> result = Math.floorMod(num1, num2);
+        }
+        return result;
+    }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,26 +1,23 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.util.Scanner; //scanner import
+import java.util.Scanner;
 
 public class CalculatorTest {
-	public static void main(String[] args) {
-		Calculator calc = new Calculator();
-		Scanner scan = new Scanner(System.in);
-		String answer;
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        String answer;
 
-		do {
-			System.out.print("Введите первое число: ");
-			calc.setNum1(scan.nextInt());
-			System.out.print("Введите знак математической операции: ");
-			calc.setOperation(scan.next().charAt(0));
-			System.out.print("Введите второе число: ");
-			calc.setNum2(scan.nextInt());
-			System.out.println("Результат: " + calc.calculate());
+        do {
+            System.out.print("Введите математическое выражение: ");
+            calc.setExpression(scan.nextLine());
 
-			do {
-				System.out.print("Хотите продолжить? [yes/no]: ");
-				answer = scan.next();
-			} while (!answer.equals("yes") && !answer.equals("no"));
-		} while (!answer.equals("no"));
-	}
+            System.out.println("Результат: " + calc.calculate());
+
+            do {
+                System.out.print("Хотите продолжить? [yes/no]: ");
+                answer = scan.nextLine();
+            } while (!answer.equals("yes") && !answer.equals("no"));
+        } while (!answer.equals("no"));
+    }
 }
